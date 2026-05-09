9477d1357f885336b8bbe22bba3513fb2674d6eb
Legendaryl <darylsumabal123@gmail.com>
2026-05-09 14:09:43 +0800

    score submit

--- app/Filament/Judge/Pages/Criteria.php
+++ app/Filament/Judge/Pages/Criteria.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Judge\Pages;
 
 use App\Models\Criteria as ModelsCriteria;
+use App\Models\Score;
+use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 use Illuminate\Support\Str;
 
@@ -25,18 +27,36 @@ class Criteria extends Page
 
     public function submit()
     {
+        try {
+            $score = collect($this->scores)->map(function ($criteria, $participantId) {
+                return [
+                    'contest_category' => $this->allCriteria->first()->criteria[0]['data']['content'],
+                    'participant_id' => $participantId,
+                    'level' => $this->allCriteria->first()->criteria[0]['data']['level'],
+                    'judge_id' => auth()->id(),
+                    'contest_id' => $this->allCriteria->first()->contest_id,
+                    'scores' => $criteria,
+                    'total_score' => array_sum($criteria),
+                    'submitted_at' => now()->toDateTimeString()
+                ];
+            })->values();
 
-        $score = collect($this->scores)->map(function ($criteria, $participantId) {
-            return [
-                'contest_category' => $this->allCriteria->first()->criteria[0]['data']['content'],
-                'participant_id' => $participantId,
-                'level' => $this->allCriteria->first()->criteria[0]['data']['level'],
-                'judge_id' => auth()->id(), // Adding global context
-                'contest_id' => $this->allCriteria->first()->contest_id,
-                'scores' => $criteria,
-                'total_score' => array_sum($criteria),
-                'submitted_at' => now()->toDateTimeString(),
-            ];
-        })->values();
+            Score::create([
+                'score' => $score->toArray(),
+            ]);
+            
+            return Notification::make()
+                ->title('Scores Submitted Successfully')
+                ->success() // Green color
+                ->body('All participant scores have been recorded.')
+                ->send();
+
+        } catch (\Exception $e) {
+            Notification::make()
+                ->title('Submission Failed')
+                ->danger() // Red color
+                ->body('An error occurred: ' . $e->getMessage())
+                ->send();
+        }
     }
 }
