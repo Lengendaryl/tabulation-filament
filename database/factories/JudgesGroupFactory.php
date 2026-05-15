b963d56b59cc05d47a4cc2977699bff981e6cfe2
Legendaryl <darylsumabal123@gmail.com>
2026-05-15 13:33:58 +0800

    Update JudgesGroupFactory.php

--- database/factories/JudgesGroupFactory.php
+++ database/factories/JudgesGroupFactory.php
@@ -19,7 +19,118 @@ class JudgesGroupFactory extends Factory
     {
         return [
             'criteria_id' => 1,
-            ''
+            [
+                'level' => 'preliminary',
+                'content' => 'Production Number',
+                'judges' => [
+                    [
+                        'status' => false,
+                        'judge_id' => 2
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 3
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 4
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 5
+                    ],
+                ],
+            ],
+            [
+                'level' => 'preliminary',
+                'content' => 'Swimwear',
+                'judges' => [
+                    [
+                        'status' => false,
+                        'judge_id' => 2
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 3
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 4
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 5
+                    ],
+                ],
+            ],
+            [
+                'level' => 'preliminary',
+                'content' => 'Casual Interview',
+                'judges' => [
+                    [
+                        'status' => false,
+                        'judge_id' => 2
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 3
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 4
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 5
+                    ],
+                ],
+            ],
+            [
+                'level' => 'preliminary',
+                'content' => 'Formal Wear',
+                'judges' => [
+                    [
+                        'status' => false,
+                        'judge_id' => 2
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 3
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 4
+                    ],
+                    [
+                        'status' => false,
+                        'judge_id' => 5
+                    ],
+                ],
+            ],
+            [
+                [
+                    'level' => 'final',
+                    'content' => 'Final Round',
+                    'judges' => [
+                        [
+                            'status' => false,
+                            'judge_id' => 2
+                        ],
+                        [
+                            'status' => false,
+                            'judge_id' => 3
+                        ],
+                        [
+                            'status' => false,
+                            'judge_id' => 4
+                        ],
+                        [
+                            'status' => false,
+                            'judge_id' => 5
+                        ],
+                    ],
+                ]
+            ]
         ];
     }
 }
