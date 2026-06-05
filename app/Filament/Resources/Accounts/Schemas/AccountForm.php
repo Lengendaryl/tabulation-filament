cd074c452905862c73118ec9a7f295bbd7342390
Legendaryl <darylsumabal123@gmail.com>
2026-06-05 17:27:09 +0800

    ok

--- app/Filament/Resources/Accounts/Schemas/AccountForm.php
+++ app/Filament/Resources/Accounts/Schemas/AccountForm.php
@@ -12,6 +12,12 @@ class AccountForm
     {
         return $schema
             ->components([
+                TextInput::make('no'),
+                Select::make('position')
+                    ->options([
+                        'Judge' => 'Judge',
+                        'Chairman of the Board of Judges' => 'Chairman of the Board of Judges',
+                    ])->required(),
                 TextInput::make('name')->required(),
                 TextInput::make('email')->unique()->required(),
                 TextInput::make('password')->minLength(8)->password()->dehydrated(fn(?string $state): bool => filled($state))->required(fn(string $operation): bool => $operation === 'create'),
