072c5ff6214be3f04432149e98bc029438f1ee88
Legendaryl <darylsumabal123@gmail.com>
2026-06-12 10:55:38 +0800

    Update Result.php

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -6,19 +6,13 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
+
 
 #[Fillable(['contest_id', 'criteria_id', 'result', 'contest_category', 'round'])]
 
 class Result extends Model
 {
-    use  LogsActivity, SoftDeletes;
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()
-            ->logAll();
-    }
+    use SoftDeletes;
 
     protected $casts = [
         'result' => 'array'
