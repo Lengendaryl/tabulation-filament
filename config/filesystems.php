3df21a09d573a77456d546a7f8e25b1ed834b6dd
Legendaryl <darylsumabal123@gmail.com>
2026-06-13 20:26:58 +0800

    ok

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,12 +41,13 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => '/storage',
+            // 'url' => '/storage',
+            'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
         ],
-// rtrim(env('APP_URL', 'http://localhost'), '/').'/storage'
+        // rtrim(env('APP_URL', 'http://localhost'), '/').'/storage'
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
