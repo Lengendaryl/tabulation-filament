feccec93d7bda092d41f8a094c1279c7e6423d27
Legendaryl <darylsumabal123@gmail.com>
2026-07-04 12:43:04 +0800

    Update SecurityHeaders.php

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -20,8 +20,19 @@ class SecurityHeaders
             header_remove('X-Powered-By');
         }
 
-        $response =  $next($request);
+        $response = $next($request);
 
+        $cspPolicy = "default-src 'self'; " .
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; " .
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
+            "img-src 'self' data: https:; " .
+            "font-src 'self' https://fonts.gstatic.com; " .
+            "connect-src 'self' https://bisu-judging-tabulation.onrender.com; " .
+            "frame-ancestors 'none'; " .
+            "object-src 'none';";
+
+        // 2. Inject the headers
+        $response->headers->set('Content-Security-Policy', $cspPolicy);
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
